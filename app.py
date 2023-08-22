03b2e1b852c24224ec2ff6af1d67f0cd7f0a38bf
sidlakkoju <siddharth.lakkoju@gmail.com>
2023-08-21 23:51:51 -0400

    Added VSS

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+from search.semantic_search import SemanticSearch
 
 app = Flask(__name__)
+
+semantic_search = SemanticSearch()
 
 @app.route('/helloWorld')  # Change the route to match your URL
 def hello():
     return "Hello World!"
+
+# Test route
+@app.route('/members')
+def get_members():
+    return {'members': ['John', 'Paul', 'George', 'Ringo']}
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    search_input = request.json['searchInput']
+    json_results = semantic_search.get_top_results_json(search_input, n=10)
+    return json_results
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)  # Listen on 0.0.0.0:8080
 
 
 # docker build --tag server-app .
-# docker run -d -p 5000:5000 server-app
+# docker run -d -p 8080:8080 server-app
---
+++ search/semantic_search.py
@@ -0,0 +1,46 @@
+import pickle
+import openai
+import numpy as np
+import json
+
+
+# Get rid later
+from search.config import openai_key
+
+openai.api_key = openai_key
+
+
+class SemanticSearch:
+    def __init__(self):
+        self.model = "text-embedding-ada-002"
+        with open('search/embedding_matrix.pkl', 'rb') as embedding_file:
+            self.embedding_matrix = pickle.load(embedding_file)
+        
+        with open('search/course_data_dict.pkl', 'rb') as data_dict_file:
+            self.course_data_dict = pickle.load(data_dict_file)
+
+
+    def get_embedding(self, text):
+        text = text.replace("\n", " ")
+        return openai.Embedding.create(input = [text], model=self.model)['data'][0]['embedding']
+    
+
+    def cosine_similarity_search(self, query_vector, embedding_matrix):
+        similarities = np.dot(self.embedding_matrix, query_vector) / (np.linalg.norm(self.embedding_matrix, axis=1) * np.linalg.norm(query_vector))
+        return similarities
+
+
+    def get_top_results_json(self, query, n=10):
+        query_vector = self.get_embedding(query)
+        similarities = self.cosine_similarity_search(query_vector, self.embedding_matrix)
+
+        top_n_indices = np.argsort(similarities)[::-1][:n]
+        top_n_data = [self.course_data_dict[index] for index in top_n_indices]
+
+        # add the similarity scores as values in the dictionaries
+        for i in range(n):
+            matrix_index = top_n_indices[i]
+            top_n_data[i]["similarity_score"] = similarities[matrix_index]
+
+        return json.dumps(top_n_data)
+
